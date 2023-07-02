dfe1128eac7befbefb5310f3f6e2e68d5bcd0ab3
Verdant31 <joao.pedro3110@hotmail.com>
2023-07-02 18:41:58 -0400

    creating table

--- src/pages/session/[sessionId].tsx
+++ src/pages/session/[sessionId].tsx
@@ -8,24 +8,71 @@ interface SessionProps {
   session: any;
 }
 
+type User = {
+  id: string;
+  name: string;
+};
 export default function Session({ session }: SessionProps) {
   const [alreadyJoined, setAlreadyJoined] = useState(false);
+  const [, setUsers] = useState<User[]>([]);
+
   useEffect(() => {
     if (!session || alreadyJoined) return;
     socket.emit("joinSession", { sessionId: session.id, user: session.user });
 
-    socket.on("joinedSession", (data) => {
-      console.log(data);
+    socket.on("joinedSession", (users) => {
+      setUsers(users);
       setAlreadyJoined(true);
     });
   }, []);
 
+  const users = [
+    { id: "1", name: "João" },
+    { id: "2", name: "Maria" },
+    { id: "3", name: "José" },
+    { id: "4", name: "Pedro" },
+    { id: "4", name: "Pedro" },
+    { id: "4", name: "Pedro" },
+  ];
+
   return (
-    <main className="mx-auto mt-32 flex w-96 flex-col items-center gap-12">
-      <h1 className="text-white">{session?.id}</h1>
-    </main>
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <main className="relative mx-auto flex w-[600px] flex-col items-center gap-12  ">
+        <button className=" absolute left-[50%] top-[50%] flex h-12 w-60 translate-x-[-50%] translate-y-[-50%] transform items-center justify-center rounded-lg bg-cyan-700">
+          <h1 className="text-md cursor-pointer font-semibold uppercase tracking-wider text-white">
+            Revelar cartas
+          </h1>
+        </button>
+        {users.length > 0 &&
+          users.map((user, index) => {
+            return (
+              <div
+                className="absolute"
+                style={getPositionByIndex(index)}
+                key={user.id}
+              >
+                <h1 className="text-xl text-white">
+                  {user.name === session.user.name ? "Eu" : user.name}
+                </h1>
+              </div>
+            );
+          })}
+      </main>
+    </div>
   );
 }
+
+const getPositionByIndex = (index: number) => {
+  const positions = [
+    { left: 0 },
+    { left: 150, bottom: 102 },
+    { right: 150, bottom: 102 },
+    { right: 0, top: 0 },
+    { right: 150, top: 100 },
+    { left: 150, top: 100 },
+  ];
+  return positions[index];
+};
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const { sessionId, userId, username } = query;
